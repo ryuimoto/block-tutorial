f6c5c34a5830e499c03613bf00937d6ba2db3274
maca <narrow617@gmail.com>
2022-04-02 17:38:38 +0900

    add game over method

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -4,20 +4,9 @@ using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
+
         Destroy(gameObject);
     }
 }
